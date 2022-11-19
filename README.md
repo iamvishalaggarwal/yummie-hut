86ec8566f64a513c41019260f885105e294280f4
Vishal Aggarwal <118606724+iamvishalaggarwal@users.noreply.github.com>
2022-11-19 20:19:15 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# Yummie Hut - Food Order Website 🍕
+
+It is a food order web app build using ReactJs Library with firebase integration for database.
+
+Techologies used : ReactJs + Firebase
+
+Various Functionalities used like :
+- react-router-dom v5 (for Routing)
+- API Fetching
+- React components and context
+- Hooks like useState(), useEffect(), useContext(), useRef()
+- Firebase integration
+- Modular css
+
+## Features
+
+- welcome landing page and designed logo
+- select food items from menu
+- cart overlay
+- add food items in cart
+- order food items from cart
+- remove and add food items in cart
+- checkout panel for ordering food
+
+
+
+## Installation
+
+Install project with npm
+
+```cmd
+  npm install 
+```
+
+For run scripts
+
+```cmd
+    npm start
+```
+    
---
+++ src/Components/Meals/MealItemForm.jsx
@@ -0,0 +1,35 @@
+import Input from "../UI/Input";
+import "./MealItemForm.css";
+import { useRef } from "react";
+
+const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value; //this value is always string so we need to convert it to a number
+    const enteredAmountNumber = +enteredAmount; //converting sttring to number
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
+  return (
+    <form className="form" onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Quantity"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
